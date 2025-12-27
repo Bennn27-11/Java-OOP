b001a8cf53702de771f061e81a5a01d71d277798
Bennn27-11 <benedictchong1127@gmail.com>
2025-12-27 15:13:51 +0800

    Add files via upload

---
+++ Product Manager/PurchaseOrder.java
@@ -0,0 +1,58 @@
+public class PurchaseOrder {
+    private String poId;
+    private String prId;
+    private String itemId;
+    private int quantity;
+    private String supplierId;
+    private String shippingStatus;
+    private String approvalStatus;
+    private String pmName;
+
+    public PurchaseOrder(String poId, String prId, String itemId, int quantity, String supplierId, String shippingStatus, String approvalStatus, String pmName) {
+        this.poId = poId;
+        this.prId = prId;
+        this.itemId = itemId;
+        this.quantity = quantity;
+        this.supplierId = supplierId;
+        this.shippingStatus = shippingStatus;
+        this.approvalStatus = approvalStatus;
+        this.pmName = pmName;
+    }
+
+    public String getPOId() {
+        return poId;
+    }
+
+    public String getPRId() {
+        return prId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public String getShippingStatus() {
+        return shippingStatus;
+    }
+    
+    public String getApprovalStatus() {
+        return approvalStatus;
+    }
+
+    public String getPmName() {
+        return pmName;
+    }
+
+    @Override
+    public String toString() {
+        return poId + ";" + prId + ";" + itemId + ";" + quantity + ";" + supplierId + ";" + shippingStatus + ";" + approvalStatus + ";" + pmName;
+    }
+}
